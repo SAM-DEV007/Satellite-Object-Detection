5a6fac73020a7d0c48ebe294c8c7a0af19b98f38
SAM <8dmasters@gmail.com>
2023-12-09 15:24:28 +0530

    Update visualize.py
    Succesfully visualized one box

--- YOLOtoCV2/visualize.py
+++ YOLOtoCV2/visualize.py
@@ -3,16 +3,52 @@ from pathlib import Path
 import cv2
 
 
+def yolobbox2bbox(coords: list, size: int) -> list:
+    x, y, w, h = coords
+
+    x1 = int((x - w / 2) * size)
+    y1 = int((x + w / 2) * size)
+    x2 = int((y - h / 2) * size)
+    y2 = int((y + h / 2) * size)
+    
+    if x1 < 0:
+        x1 = 0
+    if y1 > size - 1:
+        y1 = size - 1
+    if x2 < 0:
+        x2 = 0
+    if y2 > size - 1:
+        y2 = size - 1
+
+    return [x1, y1, x2, y2]
+
+
+def load_annotation(path: str) -> list:
+    with open(path, 'r') as f:
+        # [Class, cx, xy, w, h] 
+        ann = [list(map(float, line.replace('\n', '').split())) for line in f]
+
+    return ann
+
+
 if __name__ == '__main__':
+    name = 'himanshu'
+    img_size = 566
+
     data_path = str(Path.cwd()) + r'\YOLOtoCV2\Sample Data'
-    image_path = data_path + r'\himanshu.jpeg'
-    annotation_path = data_path + r'\himanshu\obj_train_data\himanshu.txt'
+    image_path = data_path + rf'\{name}.jpeg'
+    annotation_path = data_path + rf'\{name}\obj_train_data\{name}.txt'
 
     image = cv2.imread(image_path)
-    image = cv2.resize(image, (566, 566)) # Resize from 2264x2264 to 566x566 px
+    image = cv2.resize(image, (img_size, img_size)) # Resize from 2264x2264 to 566x566 px
+
+    annotation_list = load_annotation(annotation_path)
+    x, y, w, h = yolobbox2bbox(annotation_list[0][1:], size=img_size)
+
+    image = cv2.rectangle(image, (x, w), (y, h), (255, 0, 0), 2)
 
     cv2.imshow('Himanshu', image)
 
-    cv2.waitKey(0)
+    cv2.waitKey(0) # Esc
 
     cv2.destroyAllWindows()
