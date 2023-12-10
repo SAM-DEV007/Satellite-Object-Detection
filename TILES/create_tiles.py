1cb0e5471cd54fa8a04e23dc0fd819a886f6cd21
SAM <8dmasters@gmail.com>
2023-12-11 00:35:18 +0530

    Update create_tiles.py
    Tiling of images and related calculations

--- TILES/create_tiles.py
+++ TILES/create_tiles.py
@@ -1,6 +1,52 @@
 from pathlib import Path
 
 
+def tile(bounds, _class, x_start, y_start, width, height):
+    x_min, y_min, x_max, y_max = bounds
+    x_min, y_min, x_max, y_max = x_min - x_start, y_min - y_start, x_max - x_start, y_max - y_start
+
+    if (x_min > width) or (x_max < 0.0) or (y_min > height) or (y_max < 0.0):
+        return None
+    
+    x_max_trunc = min(x_max, width) 
+    x_min_trunc = max(x_min, 0) 
+    if (x_max_trunc - x_min_trunc) / (x_max - x_min) < 0.3:
+        return None
+
+    y_max_trunc = min(y_max, width) 
+    y_min_trunc = max(y_min, 0) 
+    if (y_max_trunc - y_min_trunc) / (y_max - y_min) < 0.3:
+        return None
+        
+    x_center = (x_min_trunc + x_max_trunc) / 2.0 / width
+    y_center = (y_min_trunc + y_max_trunc) / 2.0 / height
+    x_extend = (x_max_trunc - x_min_trunc) / width
+    y_extend = (y_max_trunc - y_min_trunc) / height
+    
+    return (_class, x_center, y_center, x_extend, y_extend)
+
+
+# https://stackoverflow.com/a/64097592/16660603
+def yolobbox2bbox(coords: list, size: int) -> list:
+    c, x, y, w, h = coords
+
+    x1 = int((x - w / 2) * size)
+    x2 = int((x + w / 2) * size)
+    y1 = int((y - h / 2) * size)
+    y2 = int((y + h / 2) * size)
+    
+    if x1 < 0:
+        x1 = 0
+    if x2 > size - 1:
+        x2 = size - 1
+    if y2 < 0:
+        y1 = 0
+    if y2 > size - 1:
+        y2 = size - 1
+
+    return [int(c), x1, y1, x2, y2]
+
+
 def load_annotation(path: str) -> list:
     with open(path, 'r') as f:
         # [Class, cx, xy, w, h] 
@@ -11,9 +57,28 @@ def load_annotation(path: str) -> list:
 
 if __name__ == '__main__':
     name = 'himanshu'
+    size = 2264 # Original size
+
+    tile_size = 566
+    tile_overlap = 64
 
     data_path = str(Path.cwd()) + r'\TILES\InputData'
     image_path = data_path + rf'\{name}.jpeg'
     annotation_path = data_path + rf'\{name}\obj_train_data\{name}.txt'
 
     annotation_list = load_annotation(annotation_path)
+    coords_list = [yolobbox2bbox(al, size=size) for al in annotation_list]
+    
+    x_tiles = (size + tile_size - tile_overlap - 1) // (tile_size - tile_overlap)
+    y_tiles = (size + tile_size - tile_overlap - 1) // (tile_size - tile_overlap)
+
+    # print(x_tiles, y_tiles)
+
+    for x in range(x_tiles):
+        for y in range(y_tiles):
+            x_end = min((x + 1) * tile_size - tile_overlap * (x != 0), size)
+            x_start = x_end - tile_size
+            y_end = min((y + 1) * tile_size - tile_overlap * (y != 0), size)
+            y_start = y_end - tile_size
+
+            # print(x_start, y_start, x_end, y_end)
