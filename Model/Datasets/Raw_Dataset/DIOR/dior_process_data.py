e8e08068763d7d65e8a2802218e0d6ffd4d5efe9
SAM <8dmasters@gmail.com>
2023-12-13 23:18:39 +0530

    Updated necessary files
    Ignored entire directory instead of individual files.
    Added a little bit of code.

---
+++ Model/Datasets/Raw_Dataset/DIOR/dior_process_data.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    tile_size = 512
+    tile_overlap = 64
+
+    data_path = str(Path.cwd()) + r'\Model\Datasets\Raw_Dataset\DIOR\Raw'
+    save_path = str(Path.cwd()) + r'\Model\Datasets\Raw_Dataset\DIOR\Processed'
