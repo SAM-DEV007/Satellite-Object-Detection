70f02bd61626dc52925c2144bfde0cee38f83738
SAM <8dmasters@gmail.com>
2023-12-15 13:01:41 +0530

    Read generated XML file

--- read_xml.py
+++ read_xml.py
@@ -1,9 +1,32 @@
 from pathlib import Path
 
 import xml.etree.ElementTree as ET
+import numpy as np
 
 
 if __name__ == '__main__':
     label_path = str(Path.cwd()) + r'\Results\label\00011.xml' # Example label path
-    
-    tree = ET.parse(label_path)
+
+    parser = ET.XMLParser(encoding = 'utf-8')
+    tree = ET.parse(label_path, parser=parser)
+    root = tree.getroot()
+
+    _names = ['data', 'xywh','xywhn', 'xyxy', 'xyxyn']
+    _obj = {}
+
+    # Class
+    _obj['class'] = [float(element.text) for element in root[0]]
+
+    # Confidence
+    _obj['confidence'] = [float(element.text) for element in root[1]]
+
+    # Original Shape
+    _obj['original_shape'] = (int(root[3][0].text), int(root[3][1].text))
+
+    # Data, xywh, xywhn, xyxy, xyxyn
+    for n in _names:
+        _obj[n] = np.array([[float(i) for i in e.text.split(' ')] for e in root.find(n)])
+
+
+    # Displays the object dictionary
+    print(_obj)
